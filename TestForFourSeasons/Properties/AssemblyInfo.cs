362e09e40a3565c9a400151657d91d20d7673347
930r934 <60056514+930r934@users.noreply.github.com>
2021-06-22 01:09:45 +0530

    5th solution test added

---
+++ TestForFourSeasons/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestForFourSeasons")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestForFourSeasons")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7873173c-ecf5-401f-b68e-080ae3c60e64")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestForFourSeasons/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit;
+using NUnit.Framework;
+using SeasonsLib;
+
+namespace TestForFourSeasons
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            SeasonTeller s = new SeasonTeller();
+            Assert.AreEqual(s.DisplaySeasonBy("JAnuary"),"Winter");
+            Assert.AreEqual(s.DisplaySeasonBy("February"), "Spring");
+            Assert.AreEqual(s.DisplaySeasonBy("MaY"), "Summer");
+            Assert.AreEqual(s.DisplaySeasonBy("July"), "Monsoon");
+        }
+    }
+}
