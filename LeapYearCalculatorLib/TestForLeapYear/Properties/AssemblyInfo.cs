553f3f61add3ff2a6479b6b3f57a1ce9260216b3
930r934 <60056514+930r934@users.noreply.github.com>
2021-06-22 13:35:36 +0530

    added 6th lib test

---
+++ LeapYearCalculatorLib/TestForLeapYear/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestForLeapYear")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestForLeapYear")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a5a0b2fb-01fc-4312-b0ff-d62d5c7b2e38")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LeapYearCalculatorLib/TestForLeapYear/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using LeapYearCalculatorLib;
+using NUnit.Framework;
+
+namespace TestForLeapYear
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            LeapYearCalculator l = new LeapYearCalculator();
+            var x = l.IsLeapYear(1200);
+            Assert.AreEqual(x, -1);
+            x = l.IsLeapYear(2000);
+            Assert.AreEqual(x, 1);
+            x = l.IsLeapYear(2001);
+            Assert.AreEqual(x, 0);
+                
+        }
+    }
+}
