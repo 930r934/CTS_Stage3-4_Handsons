6d8a6821894a791bd10829e8dbe822eeb52d6f9a
930r934 <60056514+930r934@users.noreply.github.com>
2021-07-05 18:01:33 +0530

    coreday3handson

---
+++ ASPcore/Day-3/Log4netHandson/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Log4netHandson.Controllers
+{
+    public class HomeController : Controller
+
+    {
+        private readonly ILog log = LogManager.GetLogger(typeof(HomeController));
+
+        public HomeController()
+        {
+            log.Info("Constructor of HomeController");
+        }
+        public ActionResult Index()
+        {
+
+            try
+            {
+                log.Info("Index Page!");
+                log.Debug("Debug Message test");
+                log.Warn("Warning Message test");
+                log.Error("Error Test Log test");
+
+            }
+            catch(Exception e)
+            {
+                log.Warn("Error Message" + e.Message);
+            }
+
+
+
+
+            return View();
+        }
+         
+        
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
